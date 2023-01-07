9adac20db9c0fb149deffea6d4aa7c55473eeb0c
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-01-07 15:43:31 -0700

    asdasd

---
+++ functions.js
@@ -0,0 +1,135 @@
+// FUNCTIONS
+
+// Draw Start Screen
+function drawStart() {
+    // Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+  
+    // Green Bars
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, cnv.width, 50);
+    ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+  
+    // Green Bar Text
+    ctx.font = "30px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("HELICOPTER GAME", 25, 35);
+    ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+    ctx.fillText("BEST: 0", cnv.width - 250, cnv.height - 15);
+  
+    // Helicopter
+    ctx.drawImage(heliImg, heli.x, heli.y);
+  
+    // Start Text
+    ctx.font = "40px Consolas";
+    ctx.fillStyle = "lightblue";
+    ctx.fillText("CLICK TO START", 350, 285)
+  
+    ctx.font = "25px Consolas";
+    ctx.fillText("CLICK AND HOLD LEFT MOUSE BUTTON TO GO UP", 100, 450);
+    ctx.fillText("RELEASE TO GO DOWN", 415, 480);
+  }
+    // Draw Game Elements
+function runGame() {
+    // LOGIC
+    moveHeli();
+
+    // DRAW
+drawGame();
+}
+
+function moveHeli() {
+    // Accelerate upward if mouse pressed
+    if (mouseIsPressed) {
+    heli.speed += -1;
+    }
+
+    // Apply Gravity (accel)
+    heli.speed += heli.accel;
+
+    // Contrain Speed (max/min)
+    if (heli.speed > 5) {
+    heli.speed = 5;
+} else if (heli.speed < -5) {
+     heli.speed = -5;
+}
+
+    // Move Helicopter by its speed
+heli.y += heli.speed;
+
+}
+
+    // Draw Game Elements
+function drawGame() {
+    // Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+  
+    // Green Bars
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, cnv.width, 50);
+    ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+  
+    // Green Bar Text
+    ctx.font = "30px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("HELICOPTER GAME", 25, 35);
+    ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+    ctx.fillText("BEST: 0", cnv.width - 250, cnv.height - 15);
+  
+    // Helicopter
+    ctx.drawImage(heliImg, heli.x, heli.y);
+
+  
+    // Draw Walls
+    drawWalls();
+}
+  
+    // Draw Game Over Screen
+    function drawGameOver() {
+        drawMainComponents();
+        drawWalls();
+        
+    // Circle around Helicopter
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(240, 270, 60, 0, 2 * Math.PI);
+    ctx.stroke();
+  
+    // Game Over Text
+    ctx.font = "40px Consolas";
+    ctx.fillStyle = "lightblue";
+    ctx.fillText("GAME OVER", 350, 285);
+}
+  
+    // HELPER FUNCTIONS
+    function drawWalls() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(wall1.x, wall1.y, wall1.w, wall1.h);
+    ctx.fillRect(wall2.x, wall2.y, wall2.w, wall2.h);
+    ctx.fillRect(wall3.x, wall3.y, wall3.w, wall3.h);
+    ctx.fillRect(wall4.x, wall4.y, wall4.w, wall4.h);
+}
+
+function drawMainComponents() {
+    // Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+  
+    // Green Bars
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, cnv.width, 50);
+    ctx.fillRect(0, cnv.height - 50, cnv.width, 50);
+  
+    // Green Bar Text
+    ctx.font = "30px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("HELICOPTER GAME", 25, 35);
+    ctx.fillText("DISTANCE: 0", 25, cnv.height - 15);
+    ctx.fillText("BEST: 0", cnv.width - 250, cnv.height - 15);
+  
+    // Helicopter
+    ctx.drawImage(heliImg, heli.x, heli.y);
+}
